389a340998af154ff3d10473d406a1b18b77b110
madhavimalgaonkar <madhavim2694@gmail.com>
2017-10-10 12:44:57 +0530

    Added css

--- profiles/bizballoon/themes/custom/bizballoon_omega/template.php
+++ profiles/bizballoon/themes/custom/bizballoon_omega/template.php
@@ -3,5 +3,16 @@
 /**
  * @file
  * Template overrides as well as (pre-)process and alter hooks for the
- * {{ THEME NAME }} theme.
+ * bizballoon_omega theme.
  */
+
+ function bizballoon_omega_preprocess_html(&$vars) {
+   // Add font awesome cdn.
+   drupal_add_css('//maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.css', array(
+     'type' => 'external'
+   ));
+
+   // Add google fonts cdn.
+  drupal_add_css('https://fonts.googleapis.com/css?family=Exo+2',array(
+    'type' => 'external'));
+ }
